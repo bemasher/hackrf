64f782b677822800939e1a589767b2c65a19adc0
bemasher <bemasher@gmail.com>
2015-07-10 21:54:27 -0600

    Simplify access to callback buffer.

--- callback.go
+++ callback.go
@@ -4,21 +4,14 @@ package hackrf
 // #cgo windows LDFLAGS: -lhackrf -L.
 // #include "hackrf.h"
 import "C"
+import "unsafe"
 
-import (
-	"reflect"
-	"unsafe"
-)
+const maxBufferLength = 262144
 
 var localCallback CallbackFunc
 
 //export go_callback
 func go_callback(transfer C.hackrf_transfer) C.int {
-	var buf []int8
-	slice := (*reflect.SliceHeader)(unsafe.Pointer(&buf))
-	slice.Data = uintptr(unsafe.Pointer(transfer.buffer))
-	slice.Len = int(transfer.valid_length)
-	slice.Cap = int(transfer.valid_length)
-
-	return C.int(localCallback(buf))
+	buf := (*[maxBufferLength]int8)(unsafe.Pointer(transfer.buffer))
+	return C.int(localCallback(buf[:transfer.valid_length]))
 }
